36952982fb2e67acc7666c94ed66cf9f49187083
eJwtjr1uwyAURt+F2YMhjWOzVR0yJVOWTgguF5tCfAnYzZ/y7nUjb5+OdI6+J9MAWMqJAo5MMtNBo3fCGdgYgc22drbj3IEDbLFtP7gRm1rYjlUsZfr1FvOqPteSmtZUP5D64n1qmz0/Cr6/lIPfIQyHz/r0PTThxwsID4pL6q2o6Z7w/wLqjHmhBegNtD37UabZRA8q4L3KqK2k3C8jUTUXzBLP2sfqSjm4SNdFxlvyGYvSE5PjHOPr9Qey61CD <evalenzuela2023@alu.uct.cl>
2023-11-26 09:50:48 -0300

    actualuzacion

--- clases.py
+++ clases.py
@@ -1,39 +1,47 @@
-import random
-import pygame
-
+# Clase Organismo
 class Organismo(pygame.sprite.Sprite):
-    def __init__(self, x, y, ancho, alto, color):
+    def __init__(self, columna, fila, ancho, alto, color):
         super().__init__()
         self.image = pygame.Surface([ancho, alto])
         self.image.fill(color)
-        self.rect = self.image.get_rect(topleft=(x, y))
+        self.columna = columna
+        self.fila = fila
+        self.rect = self.image.get_rect(topleft=(columna * tamaño_celda, fila * tamaño_celda))
         self.vida = True
         self.energia = 100
-        self.velocidad = random.randint(1, 5)
-
-    def mover(self, limites_pantalla):
-        direcciones = [-1, 0, 1]
-        dx = random.choice(direcciones) * self.velocidad
-        dy = random.choice(direcciones) * self.velocidad
-
-        if self.rect.left + dx < 0 or self.rect.right + dx > limites_pantalla[0]:
-            dx = -dx
-        if self.rect.top + dy < 0 or self.rect.bottom + dy > limites_pantalla[1]:
-            dy = -dy
-
-        self.rect.x += dx
-        self.rect.y += dy
 
     def update(self):
-        self.mover((pantalla_ancho, pantalla_alto))
+        pass
 
+# Subclase Planta
+class Planta(Organismo):
+    def __init__(self, columna, fila, ancho, alto, color):
+        super().__init__(columna, fila, ancho, alto, color)
+
+    def update(self):
+        pass
+
+# Subclase Animal
 class Animal(Organismo):
-    def __init__(self, x, y, ancho, alto, color, especie, dieta):
-        super().__init__(x, y, ancho, alto, color)
+    def __init__(self, columna, fila, ancho, alto, color, especie, dieta):
+        super().__init__(columna, fila, ancho, alto, color)
         self.especie = especie
         self.dieta = dieta
+        self.contador_movimiento = 0
+        self.tiempo_para_moverse = 60  # Ajusta este valor para cambiar la velocidad
 
-class Planta(Organismo):
-    def __init__(self, x, y, ancho, alto, color):
-        super().__init__(x, y, ancho, alto, color)
-        self.velocidad = 0
+    def mover(self):
+        if self.contador_movimiento >= self.tiempo_para_moverse:
+            direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # izquierda, derecha, arriba, abajo
+            dx, dy = random.choice(direcciones)
+            nueva_columna = self.columna + dx
+            nueva_fila = self.fila + dy
+            if 0 <= nueva_columna < columnas and 0 <= nueva_fila < filas:
+                self.columna = nueva_columna
+                self.fila = nueva_fila
+                self.rect.topleft = (self.columna * tamaño_celda, self.fila * tamaño_celda)
+            self.contador_movimiento = 0
+
+    def update(self):
+        self.contador_movimiento += 1
+        self.mover()
