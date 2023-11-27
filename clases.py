e8a47579934679493a322fbd09416df2029c1b58
eJwtjr1uwyAURt+F2YMhjWOzVR0yJVOWTgguF5tCfAnYzZ/y7nUjb5+OdI6+J9MAWMqJAo5MMtNBo3fCGdgYgc22drbj3IEDbLFtP7gRm1rYjlUsZfr1FvOqPteSmtZUP5D64n1qmz0/Cr6/lIPfIQyHz/r0PTThxwsID4pL6q2o6Z7w/wLqjHmhBegNtD37UabZRA8q4L3KqK2k3C8jUTUXzBLP2sfqSjm4SNdFxlvyGYvSE5PjHOPr9Qey61CD <evalenzuela2023@alu.uct.cl>
2023-11-27 16:36:10 -0300

    correccion de clase
    corregimos la clase animal

--- clases.py
+++ clases.py
@@ -23,27 +23,36 @@ class Planta(Organismo):
         pass
 
 class Animal(Organismo):
-    def __init__(self, columna, fila, ancho, alto, color, especie, dieta):
-        super().__init__(columna, fila, ancho, alto, color)
+    def __init__(self, columna, fila, ancho, alto, especie, dieta, sprite_index, columnas, filas, tamaño_celda):
+        super().__init__(columna, fila, ancho, alto, tamaño_celda)
         self.especie = especie
         self.dieta = dieta
         self.contador_movimiento = 0
         self.tiempo_para_moverse = 60
-
+        self.columnas = columnas
+        self.filas = filas
+        
+        # Asignar el sprite correspondiente al animal
+        if 0 <= sprite_index < len(animal_sprites):
+            self.image = animal_sprites[sprite_index]
+        else:
+            raise IndexError("Sprite index fuera de rango")
+        
     def mover(self):
         if self.contador_movimiento >= self.tiempo_para_moverse:
             direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1)]
             dx, dy = random.choice(direcciones)
             nueva_columna = self.columna + dx
             nueva_fila = self.fila + dy
-            if 0 <= nueva_columna < columnas and 0 <= nueva_fila < filas:
+            if 0 <= nueva_columna < self.columnas and 0 <= nueva_fila < self.filas:
                 self.columna = nueva_columna
                 self.fila = nueva_fila
-                self.rect.topleft = (self.columna * tamaño_celda, self.fila * tamaño_celda)
+                self.rect.topleft = (self.columna * self.tamaño_celda, self.fila * self.tamaño_celda)
             self.contador_movimiento = 0
         else:
             self.contador_movimiento += 1
-
+    def dibujar(self, pantalla):
+        pantalla.blit(self.image, self.rect)
     def update(self):
         self.mover()
 
