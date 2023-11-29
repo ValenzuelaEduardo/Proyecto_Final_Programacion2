97e02e15e56d9255ca7c5b9e381100afb1419240
eJwtjr1uwyAURt+F2YMhjWOzVR0yJVOWTgguF5tCfAnYzZ/y7nUjb5+OdI6+J9MAWMqJAo5MMtNBo3fCGdgYgc22drbj3IEDbLFtP7gRm1rYjlUsZfr1FvOqPteSmtZUP5D64n1qmz0/Cr6/lIPfIQyHz/r0PTThxwsID4pL6q2o6Z7w/wLqjHmhBegNtD37UabZRA8q4L3KqK2k3C8jUTUXzBLP2sfqSjm4SNdFxlvyGYvSE5PjHOPr9Qey61CD <evalenzuela2023@alu.uct.cl>
2023-11-29 07:51:06 -0300

    update main evalenzuela

--- testEduardo/ejecutaEste.py
+++ testEduardo/ejecutaEste.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-import sys
+import pygame.time
 
 # Inicialización de Pygame
 pygame.init()
@@ -16,51 +16,22 @@ TAMANO_CELDA = 40
 IMAGENES = {
     0: pygame.image.load('assets/map_images/pasto.png'),   # Pasto
     1: pygame.image.load('assets/map_images/planta.png'),    # Agujero
-    3: pygame.image.load('assets/map_images/AguaSupL.png'), 
-    4: pygame.image.load('assets/map_images/AguaSup.png'), 
-    5: pygame.image.load('assets/map_images/AguaSupR.png'), 
-    6: pygame.image.load('assets/map_images/AguaL.png'), 
-    7: pygame.image.load('assets/map_images/Agua.png'), 
-    8: pygame.image.load('assets/map_images/AguaR.png'), 
-    9: pygame.image.load('assets/map_images/AguaAbajoL.png'), 
-    10: pygame.image.load('assets/map_images/AguaAbajo.png'), 
-    11: pygame.image.load('assets/map_images/AguaAbajoR.png'), 
+    2: pygame.image.load('assets/map_images/arbusto.png'),    # Arbusto
+    3: pygame.image.load('assets/map_images/AguaSupL.png'),
+    4: pygame.image.load('assets/map_images/AguaSup.png'),
+    5: pygame.image.load('assets/map_images/AguaSupR.png'),
+    6: pygame.image.load('assets/map_images/AguaL.png'),
+    7: pygame.image.load('assets/map_images/Agua.png'),
+    8: pygame.image.load('assets/map_images/AguaR.png'),
+    9: pygame.image.load('assets/map_images/AguaAbajoL.png'),
+    10: pygame.image.load('assets/map_images/AguaAbajo.png'),
+    11: pygame.image.load('assets/map_images/AguaAbajoR.png'),
+    12: pygame.image.load('assets/map_images/planta1.png'),
 }
 
 for key, image in IMAGENES.items():
     IMAGENES[key] = pygame.transform.scale(image, (TAMANO_CELDA, TAMANO_CELDA))
 
-SPRITE_PLANTAS={
-    1: pygame.image.load('assets/map_images/planta.png'),
-    2: pygame.image.load('assets/map_images/arbusto.png'),      
-    12: pygame.image.load('assets/map_images/planta1.png'),     
-}
-
-for key, image in SPRITE_PLANTAS.items():
-    SPRITE_PLANTAS[key] = pygame.transform.scale(image, (TAMANO_CELDA, TAMANO_CELDA))
-
-# Cargar y escalar imágenes
-IMAGENES_NOCHE = {
-    0: pygame.image.load('assets/noche/pasto.png'),   # Pasto
-    1: pygame.image.load('assets/noche/planta.png'),    # Agujero
-    2: pygame.image.load('assets/noche/arbusto.png'),    # Arbusto
-    3: pygame.image.load('assets/noche/AguaSupL.png'), 
-    4: pygame.image.load('assets/noche/AguaSup.png'), 
-    5: pygame.image.load('assets/noche/AguaSupR.png'), 
-    6: pygame.image.load('assets/noche/AguaL.png'), 
-    7: pygame.image.load('assets/noche/Agua.png'), 
-    8: pygame.image.load('assets/noche/AguaR.png'), 
-    9: pygame.image.load('assets/noche/AguaAbajoL.png'), 
-    10: pygame.image.load('assets/noche/AguaAbajo.png'), 
-    11: pygame.image.load('assets/noche/AguaAbajoR.png'), 
-    12: pygame.image.load('assets/noche/planta1.png'), 
-    
-}
-
-for key, image in IMAGENES_NOCHE.items():
-    IMAGENES_NOCHE[key] = pygame.transform.scale(image, (TAMANO_CELDA, TAMANO_CELDA))
-
-    
 SPRITES_ANIMALES = {
     'depredador1': pygame.image.load('assets/depredador/depredador1.png'),
     'depredador2': pygame.image.load('assets/depredador/depredador2.png'),
@@ -79,31 +50,29 @@ SPRITES_ANIMALES = {
 for key, image in SPRITES_ANIMALES.items():
     SPRITES_ANIMALES[key] = pygame.transform.scale(image, (TAMANO_CELDA, TAMANO_CELDA))
 
-
-
 # Crear la ventana
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 boards = [
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # Fila 1
-    [0,  0,  0,  0, 0, 0, 0, 0, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4], # Fila 2
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #3
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #4
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #5
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#6
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#7
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#8
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#9
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#10
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#11
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#12
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#13
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#14
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#15
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#16
-    [0,  3,  4,  4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#17
-    [0,  6,  7,  7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],#18
-    [0,  9,  10, 10, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4], # Fila 19
-    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # Fila 20
+    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 1
+    [0,  1,  1,  1, 0, 2, 2, 0, 3, 4, 0, 1, 1, 1, 0, 2, 2, 0, 3, 4],  # Fila 2
+    [0,  0,  0,  0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 3
+    [0,  0,  0,  0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 4
+    [0,  0,  0,  12, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 5
+    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 6
+    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 7
+    [0,  0,  0,  0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 8
+    [0,  0,  0,  0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 9
+    [0,  0,  0,  0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 10
+    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 11
+    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 12
+    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 13
+    [0,  0,  0,  12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 14
+    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 15
+    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 16
+    [0,  3,  4,  4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 17
+    [0,  6,  7,  7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Fila 18
+    [0,  9,  10, 10, 11, 2, 2, 2, 2, 2, 0, 1, 1, 1, 0, 2, 2, 0, 3, 4],  # Fila 19
+    [0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # Fila 20
 ]
 # Asignamos la matriz a una variable para facilitar su uso
 matriz = boards
@@ -231,34 +200,30 @@ for i in range(4):
 
 class Planta(Organismo):
     def __init__(self, posicion, vida, energia):
-        super().__init__(posicion, vida, energia, velocidad=0)  # Las plantas no se mueven
+        super().__init__(posicion, vida, energia, velocidad=0)
         self.tipo = 'planta'
-        self.sprite = SPRITE_PLANTAS.get(f'{vida}{energia}', None)
 
     def fotosintesis(self):
-        # Asumimos que la fotosíntesis siempre es exitosa por simplicidad
-        self.vida += 1  # La planta gana 1 de vida
-        self.energia += 1  # También podría ganar energía si así lo deseas
-
-    def dibujar_pl(self, pantalla):
-        if self.sprite:
-            # Dibuja el sprite en la posición correspondiente
-            pantalla.blit(self.sprite, (self.posicion[0] * TAMANO_CELDA, self.posicion[1] * TAMANO_CELDA))
-        else:
-            # Si no hay sprite, podrías dibujar un rectángulo o círculo como placeholder
-            pygame.draw.rect(pantalla, (20, 255, 0), (self.posicion[0] * TAMANO_CELDA, self.posicion[1] * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA))
+        self.vida += 1
+        self.energia += 1
 
     def reproducirse(self):
-        # Puedes agregar la lógica de reproducción aquí
         pass
 
 posiciones_de_plantas = []
-
+# Ejemplo de cómo podrías actualizar la vida de las plantas en el juego
+# Suponiendo que tienes una lista de todas las plantas en el juego
 plantas = [Planta((x, y), vida=5, energia=10) for x, y in posiciones_de_plantas]
 
 # Después de cada ciclo en el juego, podrías llamar a la fotosíntesis para cada planta
 for planta in plantas:
     planta.fotosintesis()
+
+
+    def reproducirse(self):
+        # Lógica para la reproducción por semillas
+        pass
+
 class Ambiente:
     def __init__(self):
         self.factores = []  # Lista de factores abióticos
@@ -267,85 +232,46 @@ class Ambiente:
         # Lógica para cambiar el clima
         pass
 
-    def dibujar_tablero(self, pantalla):
-        for y, fila in enumerate(matriz):
-            for x, valor in enumerate(fila):
-                imagen = IMAGENES.get(valor)
-                if imagen:
-                    pantalla.blit(imagen, (x * TAMANO_CELDA, y * TAMANO_CELDA))
-
 class Ecosistema:
     def __init__(self):
         self.organismos = []
         self.ambiente = Ambiente()
 
-    def agregar_organismo(self, organismo):
-        """Agrega un organismo al ecosistema."""
-        self.organismos.append(organismo)
+    def dibujar_tablero(self, pantalla):  # Agrega 'self' y 'pantalla' como argumentos
+        for y, fila in enumerate(matriz):
+            for x, valor in enumerate(fila):
+                imagen = IMAGENES.get(valor)
+                if imagen:
+                    pantalla.blit(imagen, (x * TAMANO_CELDA, y * TAMANO_CELDA))  # Asegúrate de multiplicar por TAMANO_CELDA
 
-    def ciclo_ecologico(self):
-        """Realiza un ciclo completo en el ecosistema."""
-        # Actualizar el clima u otros factores abióticos
-        self.ambiente.cambiar_clima()
-
-        # Actualizar los organismos en el ecosistema
-        for organismo in self.organismos:
-            organismo.actuar(self.organismos)
-
-        # Eliminar organismos que han muerto
-        self.organismos = [organismo for organismo in self.organismos if organismo.esta_vivo()]
-
-        # Puedes agregar más lógica según sea necesario para mantener el equilibrio ecológico
-
-    def dibujar_ecosistema(self, pantalla):
-        """Dibuja el estado actual del ecosistema en la pantalla."""
-        pantalla.fill((0, 0, 0))  # Limpia la pantalla antes de dibujar el nuevo frame
-
-        # Dibujar el tablero desde el ambiente
-        self.ambiente.dibujar_tablero(pantalla)
-
-        # Dibujar organismos
-        for organismo in self.organismos:
-            organismo.dibujar(pantalla)
+def actualizar_animales(lista_animales, matriz):
+    for animal in lista_animales:
+        if animal.especie.startswith('depredador'):
+            animal.mover(matriz, 3)  # Rango de movimiento para depredadores
+        else:
+            animal.mover(matriz, 1)  # Rango de movimiento para animales normales
 
 # Iniciar el ecosistema
 ecosistema = Ecosistema()
 
-# Variable para controlar el tiempo transcurrido
-tiempo_transcurrido = 0
-tiempo_cambio_clima = 10000  # 30 segundos en milisegundos
-
 # Bucle principal
 corriendo = True
 reloj = pygame.time.Clock()
-
 while corriendo:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             corriendo = False
         elif evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_SPACE:
-                # Agrega aquí la lógica que deseas ejecutar cuando se presiona la tecla ESPACIO
                 pass
 
-    # Medir el tiempo transcurrido
-    tiempo_transcurrido += reloj.tick()
-
-    # Cambiar el clima cada 30 segundos
-    if tiempo_transcurrido >= tiempo_cambio_clima:
-        ecosistema.ambiente.cambiar_clima()  # Acceder al ambiente a través del ecosistema
-        tiempo_transcurrido = 0  # Reiniciar el contador de tiempo
-
-    pantalla.fill((0, 0, 0))  # Limpia la pantalla antes de dibujar el nuevo frame
-
-    # Dibujar el ecosistema
-    ecosistema.dibujar_ecosistema(pantalla)
-
-    # Resto del bucle principal (actualización de estado, dibujo, etc.)
     for planta in plantas:
-        planta.fotosintesis()  # Actualizar la vida de las plantas
-        planta.dibujar_pl(pantalla)
-        
+        planta.fotosintesis()
+
+    pantalla.fill((0, 0, 0))
+
+    ecosistema.dibujar_tablero(pantalla)
+
     for depredador in [animal for animal in lista_animales if isinstance(animal, Depredador)]:
         depredador.mover(matriz)
         
@@ -355,7 +281,9 @@ while corriendo:
 
     for animal in lista_animales:
         animal.dibujar(pantalla)
-    pygame.display.flip()  # Actualiza la pantalla completa
+
+    pygame.display.flip()
+    
+    reloj.tick(30)
 
 pygame.quit()
-sys.exit()
